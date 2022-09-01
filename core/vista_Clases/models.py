862bf68c6f3b2a16fac1c412b1d05eef739ff0c7
Juan castro <95278683+jmcb7344@users.noreply.github.com>
2022-09-01 10:38:33 -0300

    Update models.py

--- core/vista_Clases/models.py
+++ core/vista_Clases/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Materia(models.Model):
-    nombre = models.CharField(max_length=25)
-    descripcion = models.TextField(null=True, blank=True) 
+    nombre = models.CharField(max_length=25, verbose_name='Nombre del curso')
+    descripcion = models.TextField(null=True, blank=True, verbose_name='Descripcion del curso')
 
     def __str__(self):
         return f'{self.nombre}'
